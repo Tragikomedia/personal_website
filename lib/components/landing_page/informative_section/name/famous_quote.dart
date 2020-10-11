f445cb3cb30cf5eae4fe3bb36b85dfbf203862c8
Tragikomedia <kamilk9625@gmail.com>
2020-10-11 17:33:56 +0200

    Added widget to show quotes.

---
+++ lib/components/landing_page/informative_section/name/famous_quote.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class FamousQuote extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(width: 450,
+    height: 50.0,
+    child: FadeAnimatedTextKit(
+      repeatForever: true,
+      duration: Duration(seconds: 5),
+      textStyle: TextStyle(fontFamily: 'AutourOne', fontSize: 20.0, color: Colors.black45),
+      text: ['"Nein, nein, nein"','Lorem Ipsum is simply dummy text of the printing and typesetting industry', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout']),);
+  }
+}
--- lib/pages/landing_page/d_landing_page.dart
+++ lib/pages/landing_page/d_landing_page.dart
@@ -2,11 +2,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:web_test/components/landing_page/informative_section/cv_button/cv_button.dart';
 import 'package:web_test/components/landing_page/informative_section/icon_row/icon_row.dart';
+import 'package:web_test/components/landing_page/informative_section/name/famous_quote.dart';
 import 'package:web_test/components/landing_page/informative_section/name/name_widget.dart';
 import 'package:web_test/components/landing_page/informative_section/navigation_buttons/navigation_button.dart';
 import 'package:web_test/components/landing_page/informative_section/navigation_buttons/navigation_row.dart';
 import 'package:web_test/components/landing_page/photo_swapper/photo_swapper.dart';
 import 'package:web_test/components/reusable/white_box_size_keeper.dart';
+import 'package:web_test/utilities/constants.dart';
 
 class DesktopLandingPage extends StatelessWidget {
   @override
@@ -15,29 +17,51 @@ class DesktopLandingPage extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Center(child: PhotoSwapper()),
+        Center(
+            child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: PhotoSwapper(),
+        )),
         // SizedBox used to enable proper Column positioning
         WhiteBoxSizeKeeper(),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            NameWidget(),
-            IconRow(),
-            NavigationRow(buttons: [
-              NavigationButton(text: 'About Me'),
-              NavigationButton(text: 'Translations'),
-            ]),
-            NavigationRow(buttons: [
-              NavigationButton(text: 'Contact'),
-              NavigationButton(text: 'Programming')
-            ]),
-            CvButton(),
-          ],
+        ConstrainedBox(
+          constraints: BoxConstraints(minHeight: 650),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              NameWidget(),
+              FamousQuote(),
+              ConstrainedBox(
+                constraints: BoxConstraints(maxHeight: 350),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ConstrainedBox(
+                      constraints:
+                          BoxConstraints(maxHeight: kDeskButtonHeight * 2 + 30),
+                      child: Column(
+                        children: [
+                          NavigationRow(buttons: [
+                            NavigationButton(text: 'About Me'),
+                            NavigationButton(text: 'Translations'),
+                          ]),
+                          NavigationRow(buttons: [
+                            NavigationButton(text: 'Contact'),
+                            NavigationButton(text: 'Programming')
+                          ]),
+                        ],
+                      ),
+                    ),
+                    CvButton(),
+                    IconRow()
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ],
     );
   }
 }
-
-
